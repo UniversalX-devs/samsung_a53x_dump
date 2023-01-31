154bee56768fa653a934675cf38380780a18d77a
ItsLynix <lynixthewolf@gmail.com>
2023-01-31 21:46:03 +0000

    Add extras for a53xnaxx-user 13 TP1A.220624.014 A536BXXU4BVJG release-keys
    Signed-off-by: ItsLynix <lynixthewolf@gmail.com>

---
+++ twrp-device-tree/samsung/a53x/vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_a53x-user
+add_lunch_combo omni_a53x-userdebug
+add_lunch_combo omni_a53x-eng
